3e863a4427347130bed5f738fc98397f0ab97bae
Michael Israel <57787893+mikelisrael@users.noreply.github.com>
2021-02-05 01:08:54 +0100

    Add files via upload
    Grocery bud react app with local storage

---
+++ build/precache-manifest.f1735f657e145282387b5b36cf857a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a3d6fec6b49eda9b6ea06aabcc704e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2452904cc5477ac054f",
+    "url": "/static/css/main.96488d99.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2452904cc5477ac054f",
+    "url": "/static/js/main.c1b4a915.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
